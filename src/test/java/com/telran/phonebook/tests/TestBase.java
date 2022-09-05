2d84e95df6abdf0b1f3cbfe73f67bbdaa51e2a87
Ira Baker <49585685+irinabaker@users.noreply.github.com>
2022-09-05 11:52:39 +0200

    Added logback.xml

--- src/test/java/com/telran/phonebook/tests/TestBase.java
+++ src/test/java/com/telran/phonebook/tests/TestBase.java
@@ -1,6 +1,8 @@
 package com.telran.phonebook.tests;
 
 import fw.ApplicationManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -8,14 +10,26 @@ public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager();
 
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
     @BeforeMethod
     public void setUp() {
         app.init();
     }
 
+    @BeforeMethod
+    public void startTest() {
+        logger.info("Start test");
+    }
+
     @AfterMethod
     public void tearDown() {
         app.stop();
+    }
+
+    @AfterMethod
+    public void stopTest() {
+        logger.info("Stop test");
     }
 
 }
